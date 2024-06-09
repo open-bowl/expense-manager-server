7bb6867016879a040a5a8226cfca8d3b22ef094e
Giridharan Rengarajan <18137429+DharanBro@users.noreply.github.com>
2024-06-09 13:20:06 +0530

    initial commit

---
+++ src/main/java/com/example/expensemanagerserver/ExpenseManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.expensemanagerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseManagerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/expensemanagerserver/ExpenseManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expensemanagerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
